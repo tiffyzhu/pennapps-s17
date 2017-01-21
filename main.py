b875937d8396e561e141ac0f0848efa75651d56f
Tiffany Zhu <tiffyzhu@gmail.com>
2017-01-20 23:19:42 -0500

    updated

--- main.py
+++ main.py
@@ -3,14 +3,10 @@
 # $ clarifai config
 # CLARIFAI_APP_ID: []: nblFxyZttaF1Nw7G1ShCSTzt-8K9joxh9VU5s0iV
 # CLARIFAI_APP_SECRET: []: 0Nux82Kte8-4Gx1H7f2SqLOKuT_vUVd7JcMO3u95
-#hi kristin here
+
 from clarifai.rest import ClarifaiApp
 
 app = ClarifaiApp()
-app.tag_urls(['https://samples.clarifai.com/metro-north.jpg'])
-
-model = app.models.get('color', model_type='color')
-
-image2 = ClImage(url='https://samples.clarifai.com/wedding.jpg')
-
-model.predict([image1, image2])
+# predict with general model
+image = 'https://samples.clarifai.com/metro-north.jpg'
+app.tag_urls([image])
