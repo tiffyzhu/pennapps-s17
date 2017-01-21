70c3d145af151eef5b778fe64f305f8516c3e74e
Tiffany Zhu <tiffyzhu@gmail.com>
2017-01-20 22:48:36 -0500

    Add existing file

---
+++ main.py
@@ -0,0 +1,16 @@
+# pip install clarifai==2.0.17
+
+# $ clarifai config
+# CLARIFAI_APP_ID: []: nblFxyZttaF1Nw7G1ShCSTzt-8K9joxh9VU5s0iV
+# CLARIFAI_APP_SECRET: []: 0Nux82Kte8-4Gx1H7f2SqLOKuT_vUVd7JcMO3u95
+
+from clarifai.rest import ClarifaiApp
+
+app = ClarifaiApp()
+app.tag_urls(['https://samples.clarifai.com/metro-north.jpg'])
+
+model = app.models.get('color', model_type='color')
+
+image2 = ClImage(url='https://samples.clarifai.com/wedding.jpg')
+
+model.predict([image1, image2])
