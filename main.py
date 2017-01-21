1328bccd6c5cc450f00ca0f6f8716e478e3840ca
Kristin Yin <kristinyin@Kristins-Device.local>
2017-01-20 22:51:09 -0500

    kristin's commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # $ clarifai config
 # CLARIFAI_APP_ID: []: nblFxyZttaF1Nw7G1ShCSTzt-8K9joxh9VU5s0iV
 # CLARIFAI_APP_SECRET: []: 0Nux82Kte8-4Gx1H7f2SqLOKuT_vUVd7JcMO3u95
-
+#hi kristin here
 from clarifai.rest import ClarifaiApp
 
 app = ClarifaiApp()
